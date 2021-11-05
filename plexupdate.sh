c782bf21a9f6043e6078e0c9c8d7b628e88b49dc
Fifteen 15 Studios <havens1515@gmail.com>
2021-11-05 13:47:25 -0400

    Update for DSM7 and implement improvements
    Adds customization via variables
    More accurate version checking
    Improved security - Only install the file that we downloaded, not any other
    Output to a log file

--- plexupdate.sh
+++ plexupdate.sh
@@ -7,27 +7,45 @@
 # @author @martinorob https://github.com/martinorob
 # https://github.com/martinorob/plexupdate/
 
-#!/bin/bash
-mkdir -p /tmp/plex/ > /dev/null 2>&1
-token=$(cat /volume1/Plex/Library/Application\ Support/Plex\ Media\ Server/Preferences.xml | grep -oP 'PlexOnlineToken="\K[^"]+')
+# Volume where Plex is installed
+VOLUME=$(echo "/volume1")
+# Location of temp folder for this script
+tmpFolder="/tmp/plex"
+resultFile=$tmpFolder/result.txt
+
+# Make temp directory if it doesn't already exist
+mkdir -p $tmpFolder/ > /dev/null 2>&1
+
+# Get information we need
+token=$(cat ${VOLUME}/PlexMediaServer/AppData/Plex\ Media\ Server/Preferences.xml | grep -oP 'PlexOnlineToken="\K[^"]+')
 url=$(echo "https://plex.tv/api/downloads/5.json?channel=plexpass&X-Plex-Token=$token")
 jq=$(curl -s ${url})
-newversion=$(echo $jq | jq -r .nas.Synology.version)
-echo New Ver: $newversion
-curversion=$(synopkg version "Plex Media Server")
-echo Cur Ver: $curversion
+
+# Get version numbers
+# For some reason the part after the dash changes, so exclude it
+newversion=$(echo ${jq} | jq -r '.nas."Synology (DSM 7)".version' | cut -d'-' -f 1)
+echo "New Ver: $newversion" > $resultFile
+curversion=$(synopkg version "PlexMediaServer" | cut -d'-' -f 1)
+echo "Cur Ver: $curversion" >> $resultFile
+
+# Compare version numbers
 if [ "$newversion" != "$curversion" ]
+# New Version Available
 then
-echo New Vers Available
-/usr/syno/bin/synonotify PKGHasUpgrade '{"[%HOSTNAME%]": $(hostname), "[%OSNAME%]": "Synology", "[%PKG_HAS_UPDATE%]": "Plex", "[%COMPANY_NAME%]": "Synology"}'
-CPU=$(uname -m)
-url=$(echo "${jq}" | jq -r '.nas.Synology.releases[] | select(.build=="linux-'"${CPU}"'") | .url')
-/bin/wget $url -P /tmp/plex/
-/usr/syno/bin/synopkg install /tmp/plex/*.spk
-sleep 30
-/usr/syno/bin/synopkg start "Plex Media Server"
-rm -rf /tmp/plex/*
+	/usr/syno/bin/synonotify PKGHasUpgrade '{"[%HOSTNAME%]": $(hostname), "[%OSNAME%]": "Synology", "[%PKG_HAS_UPDATE%]": "PlexMediaServer", "[%COMPANY_NAME%]": "Plex Inc"}'
+	CPU=$(uname -m)
+	url=$(echo "${jq}" | jq -r '.nas."Synology (DSM 7)".releases[] | select(.build=="linux-'"${CPU}"'") | .url')
+	# Download the file
+	/bin/wget $url -P $tmpFolder/
+	# Get filename from URL
+	slashes=$(awk -F"/" '{print NF-1}' <<< "${url}")
+	filename=$(echo $url | cut -d '/' -f $(expr $slashes + 1))
+	# Install the file
+	/usr/syno/bin/synopkg install $tmpFolder/$filename >> $resultFile
+	sleep 30
+	/usr/syno/bin/synopkg start "Plex Media Server"
+	rm -rf $tmpFolder/*.spk
 else
-echo No New Ver
+	echo "No new version available" >> $resultFile
 fi
 exit
